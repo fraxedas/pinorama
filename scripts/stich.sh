22aa3a33a26e6032fbccb9c1e2834dd4fbc62bb6
Oscar Fraxedas <fraxedas@gmail.com>
2014-12-15 16:23:37 -0500

    stick the time-lapse

---
+++ scripts/stich.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Requires mencoder
+#sudo apt-get install mencoder
+
+#Timelapse parameters
+FPS=30
+DATE=$(date +"%Y-%m-%d_%H%M%S")
+
+#Get the list of files
+ls *.jpg > stills.txt
+
+#Encode the video
+mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse_$DATE.avi -mf type=jpeg:fps=$FPS mf://@stills.txt
