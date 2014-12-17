14e068bf96dc35b5ae6977493b8fb80ca07376ab
fraxedas <fraxedas@gmail.com>
2014-12-17 17:55:36 -0500

    Add pulse with modulation

--- scripts/servo.py
+++ scripts/servo.py
@@ -5,12 +5,18 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(7, GPIO.OUT)
 
+p = GPIO.PWM(7,50)
+p.start(7.5)
+
 try:
 	while True:
-		GPIO.output(7, 1)
-		time.sleep(0.0015)
-		GPIO.output(7, 0)
+		p.ChangeDuttyCycle(7.5)
+		time.sleep(1)
+		p.ChangeDuttyCycle(12.5)
+		time.sleep(1)
+		p.ChangeDuttyCycle(2.5)
+		time.sleep(1)
 		
-		time.sleep(2)
 except KeyboardInterrupt:
+	p.stop()
 	GPIO.cleanup()
