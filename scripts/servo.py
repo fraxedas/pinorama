054452a81eef157fe1ee6c8e1f72786c9009d388
Oscar Fraxedas <fraxedas@gmail.com>
2014-12-26 13:07:43 -0500

    control for both servos
    pan and tilt

--- scripts/servo.py
+++ scripts/servo.py
@@ -4,22 +4,30 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(7, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
-p = GPIO.PWM(7,50)
-p.start(7.5)
+pan = GPIO.PWM(7,50)
+pan.start(7.5)
+
+tilt = GPIO.PWM(7,50)
+tilt.start(7.5)
 
 try:
     while True:
         #Neutral
-        p.ChangeDutyCycle(7.5)
+        pan.ChangeDutyCycle(7.5)
+        tilt.ChangeDutyCycle(7.5)
         time.sleep(1)
         #180
-        p.ChangeDutyCycle(12.5)
+        pan.ChangeDutyCycle(12.5)
+        tilt.ChangeDutyCycle(12.5)
         time.sleep(1)
         #0
-        p.ChangeDutyCycle(2.5)
+        pan.ChangeDutyCycle(2.5)
+        tilt.ChangeDutyCycle(2.5)
         time.sleep(1)
 
 except KeyboardInterrupt:
-    p.stop()
+    pan.stop()
+    tilt.stop()
     GPIO.cleanup()
