dad9953fcc8e931d5252dc52ccce63454e85f118
Oscar Fraxedas <fraxedas@gmail.com>
2015-01-27 16:26:19 -0500

    cleanup the stitch script

--- scripts/stich.sh
+++ scripts/stich.sh
@@ -12,3 +12,4 @@ ls *.jpg > stills.txt
 #Encode the video
 mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse_$DATE.avi -mf type=jpeg:fps=$FPS mf://@stills.txt
 echo timelapse_$DATE.avi
+rm stills.txt
