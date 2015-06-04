8b0f2fda3095bad11355326bb2c5677886098901
Oscar Fraxedas <fraxedas@gmail.com>
2015-06-03 21:31:57 -0400

    node setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,6 +3,8 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install fbi
 sudo apt-get install omxplayer
+
+curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get install nodejs
-sudo apt-get install npm
 sudo npm install -g forever
+sudo npm install -g grunt-cli
